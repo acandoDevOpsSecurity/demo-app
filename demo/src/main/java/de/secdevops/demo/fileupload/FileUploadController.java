5b81fdbd45071d09973b2465815e620c00e6d6c6
Benjamin Schnarr <benjamin.schnarr@cgi.com>
2020-01-03 11:31:52 +0100

    allow path traversal attack for workshop

--- demo/src/main/java/de/secdevops/demo/fileupload/FileUploadController.java
+++ demo/src/main/java/de/secdevops/demo/fileupload/FileUploadController.java
@@ -1,10 +1,12 @@
 package de.secdevops.demo.fileupload;
 
-import de.secdevops.AppsensorUtils;
-import de.secdevops.user.UserUtils;
-import org.owasp.appsensor.core.DetectionPoint.Category;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,10 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import de.secdevops.AppsensorUtils;
+import de.secdevops.user.UserUtils;
 
 @Controller
 public class FileUploadController {
@@ -35,11 +35,11 @@ public class FileUploadController {
 	public ModelAndView upload(@RequestParam(value = "filename", required = true) String filename, MultipartFile file) throws Exception {
 
 		Path path = Paths.get(filename);
-		if (filename.contains("/")){
-			//appsensorUtils.addAttack(Category.REQUEST, "RE8"); //https://www.owasp.org/index.php/AppSensor_DetectionPoints#RE8:_Unexpected_Type_of_Characters_in_Parameter
-			appsensorUtils.addEvent(Category.REQUEST, "RE8");
-			throw new AccessDeniedException("Nice try.");
-		}
+//		if (filename.contains("/")){
+//			//appsensorUtils.addAttack(Category.REQUEST, "RE8"); //https://www.owasp.org/index.php/AppSensor_DetectionPoints#RE8:_Unexpected_Type_of_Characters_in_Parameter
+//			appsensorUtils.addEvent(Category.REQUEST, "RE8");
+//			throw new AccessDeniedException("Nice try.");
+//		}
 		path = Files.write(path, file.getBytes());
 
 		ModelAndView mav = new ModelAndView("upload-done");
